c23cd49abaf13248a2b8c9f2753c15d0853c5757
Reynald Ramirez <rramirez@tredium.com>
2022-08-23 16:16:03 -0400

    [U] Updates in the branch with one little sample

---
+++ free_components_samples/Store.tsx
@@ -0,0 +1,16 @@
+// https://www.youtube.com/watch?v=lATafp15HWA
+import{Col,Row}from"react-bootstrap"
+import storeItems from"../data/items.json"
+
+export function Store(){
+  return(
+    <h1>Store</h1>
+        <Row md={2} xs={1} lg={3} className="g-3">
+            {
+                storeItems.map(item=>(
+                    <Col>{JSON.stringify(item)}</Col>
+                ))
+            }
+        </Row>
+	)
+}
